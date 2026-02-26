18b513b82ae4b0a76fee817a077a72d89be202d7
Ben <brainfuel@icloud.com>
2026-02-27 05:27:47 +0700

    Initial Commit

---
+++ AI Tools/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AI Tools
+//
+//  Created by Ben Milford on 27/02/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
